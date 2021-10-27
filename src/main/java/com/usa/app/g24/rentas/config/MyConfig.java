e4559697a9e0a55813f442511497b9412476ad28
juanrodriguez11mt <juan.rodriguez11.mt@correo.usa.edu.co>
2021-10-27 06:55:17 -0500

    Cors

---
+++ src/main/java/com/usa/app/g24/rentas/config/MyConfig.java
@@ -0,0 +1,24 @@
+package com.usa.app.g24.rentas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@SuppressWarnings("deprecation")
+@Configuration
+public class MyConfig {
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE");
+            }
+        };
+    }
+	
+}
+
